a9bc9f79ac7c469a802851bd9ef6ed4312f8ae34
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-17 13:44:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
-sudo ./zerotier.sh
+#sudo ./zerotier.sh
 #sudo ./samba.sh
