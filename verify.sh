f08715ca6fabadf3e770b9a6155f165404d289f7
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-18 20:42:43 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+pularLinha=0
+
 if [ ! -e '.customize_environment' ]; then
+   pularLinha=1
    echo -e "$1 .customize_environment not found!"
 fi
 
 if [ ! -e 'CORE/build/Hash-Maker' ]; then
+   pularLinha=1
    echo -e "$1 Hash-Maker not found!"
 fi
 
 if [ ! -e 'CORE/build/config.json' ]; then
+   pularLinha=1
    echo -e "$1 config.json not found!"
+fi
+
+if [ pularLinha = 1 ]; then
+    echo -e "\n"
 fi
