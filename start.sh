4c0dd5ffa31902e7beff03fb83468bffa7592ef7
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-18 17:51:14 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,6 +17,9 @@ wget https://raw.githubusercontent.com/robotpack/scripts/master/zerotier.sh && c
 rm -rf samba.sh
 wget https://raw.githubusercontent.com/robotpack/scripts/master/samba.sh && chmod 777 samba.sh
 
+rm -rf init.sh
+wget https://raw.githubusercontent.com/robotpack/scripts/master/init.sh && chmod 777 init.sh
+
 rm -rf make.sh
 wget https://raw.githubusercontent.com/robotpack/scripts/master/make.sh && chmod 777 make.sh && ./make.sh
 
