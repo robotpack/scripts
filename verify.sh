43b6029fe00b46ce786b35ab7a060adbe88ebeb7
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-18 21:35:48 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -5,6 +5,8 @@ pularLinha='F'
 if [ ! -e '.customize_environment' ]; then
    pularLinha='T'
    echo -e "$1 .customize_environment not found!"
+   wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh && chmod 777 start.sh && ./start.sh 
+   exit
 fi
 
 if [ ! -e 'CORE/build/Hash-Maker' ]; then
