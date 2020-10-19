20e19abb377255d9b30eae3615cb4ccd0777faea
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-18 23:31:27 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -5,7 +5,9 @@ pularLinha='F'
 if [ ! -e '.customize_environment' ]; then
    pularLinha='T'
    echo -e "$1 .customize_environment not found!"
-   wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh && chmod 777 start.sh && ./start.sh 
+   wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh
+   chmod 777 start.sh
+   ./start.sh 
    exit
 fi
 
