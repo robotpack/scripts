a3e204c6a5c23952baa29b9410ce0d5f8a648c06
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-19 00:24:30 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,16 +3,16 @@
 export TZ=America/Sao_Paulo
 echo "$(pwd)" > home.file
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/.customize_environment && echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/.customize_environment && echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/install.sh && chmod 777 install.sh
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/install.sh && chmod 777 install.sh
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/zerotier.sh && chmod 777 zerotier.sh
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/zerotier.sh && chmod 777 zerotier.sh
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/samba.sh && chmod 777 samba.sh
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/samba.sh && chmod 777 samba.sh
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/init.sh && chmod 777 init.sh
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/init.sh && chmod 777 init.sh
 
-wget -N https://raw.githubusercontent.com/robotpack/scripts/master/make.sh && chmod 777 make.sh && ./make.sh
+wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/make.sh && chmod 777 make.sh && ./make.sh
 
 echo 'start.sh ok!'
