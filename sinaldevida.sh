7195253346d523c4d12756334bafde766b505e21
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-19 23:51:07 -0300

    Update sinaldevida.sh

--- sinaldevida.sh
+++ sinaldevida.sh
@@ -4,6 +4,7 @@ export TZ=America/Sao_Paulo
 
 padrao="\033[0m"
 verde="\033[0;32m"
+vermelho="\e[31m"
 
 while true
 do
@@ -21,12 +22,14 @@ do
   if echo "$conta" | grep -q "$1"; then
 
      command=""
-     command=" ${command} echo -e '"${verde}"'ON'"${padrao}"' ;"
-     command=" ${command} [ -e 'CORE/build/Hash-Maker' ]  && echo -e '"${verde}"'OK'"${padrao}"' ;"
-     command=" ${command} [ -e '.customize_environment' ] && echo -e '"${verde}"'OK'"${padrao}"' ;"
-     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ] && rm -rf * && wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh &&" 
-     command=" ${command} chmod 777 start.sh && ./start.sh $conta ;"
+     command=" ${command} echo -e '"${verde}"'$conta ON'"${padrao}"' ;"
+     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ]  && echo -e '"${vermelho}"'$conta Hash-Maker not found!'"${padrao}"' ;"
+     command=" ${command} [ ! -e '.customize_environment' ] && echo -e '"${vermelho}"'$conta .customize_environment not found!'"${padrao}"' ;"
+
+     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ] && rm -rf * && wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh &&"
+     command=" ${command} chmod 777 start.sh && ./start.sh $conta &&"
      command=" ${command} sleep 1 ; exit"
+
 
      echo
      echo $conta
