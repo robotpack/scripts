075201885e16b3de47fdcca3a45567aa5b1ad1ef
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-20 00:07:22 -0300

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-time=2
+secs=2
 
 tmux new -s _proxy
-sleep $time
+sleep $secs
 tmux new -s "u3f8u1" -d "./sinaldevida.sh 'u3f8u1'"
-sleep $time
+sleep $secs
 tmux new -s "c3a8h2" -d "./sinaldevida.sh 'c3a8h2'"
-sleep $time
+sleep $secs
 tmux new -s "h2u6x0" -d "./sinaldevida.sh 'h2u6x0'"
-sleep $time
+sleep $secs
 tmux new -s "g7n0z2" -d "./sinaldevida.sh 'g7n0z2'"
-sleep $time
+sleep $secs
 tmux new -s "p1z6c9" -d "./sinaldevida.sh 'p1z6c9'"
-sleep $time
+sleep $secs
 tmux new -s "g3x8z0" -d "./sinaldevida.sh 'g3x8z0'"
-sleep $time
+sleep $secs
 tmux attach -t _proxy
