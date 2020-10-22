529a82bf4e880e7ef41ba759df761c7165be034a
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-22 09:59:50 -0300

    Create sinaldevida_host1.sh

---
+++ sinaldevida_host1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export TZ=America/Sao_Paulo
+data=$(date +"%d/%m/%Y %H:%M:%S")
+
+padrao="\033[0m"
+verde="\033[0;32m"
+vermelho="\e[31m"
+
+while true
+do
+
+ sudo rm -rf .ssh/known_hosts
+
+ echo
+ echo 'Lendo lista de contas...'
+ gcloud auth list --format="value(account)" > contas.txt
+ lista=( `cat "contas.txt" `)
+
+ for conta in "${lista[@]}"
+  do
+
+  if echo "$conta" | grep -q "$1"; then
+
+     email=$conta
+     session=${email/.*/}
+
+     echo
+     echo $conta
+     echo "$(date)"
+     tmux kill-window -t $session
+     tmux new -s $session -d "./gcloud_login.sh '$conta'"
+     sleep 1
+
+  fi
+
+ done
+
+done
