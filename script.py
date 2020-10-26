26106e29459019119c9e7dae5801cf51739dde0e
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-26 18:44:01 -0300

    Update script.py

--- script.py
+++ script.py
@@ -62,7 +62,7 @@ for contas in listaContas :
     print("Instalando " + user )
     os.system("tmux new -s " + user + " -d " ) 
     time.sleep(2)
-    os.system("tmux send-keys -t " + user + " ' ./cloud-shell.sh ' " + conta + "' C-m '" )
+    os.system("tmux send-keys -t " + user + " ' ./cloud-shell.sh ' " + conta + " C-m " )
     time.sleep(4) #Para o processo nao morrer
     print("OK!")
 
