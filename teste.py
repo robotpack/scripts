f4ec82d28e311d0d093c9d7526592d1a646d8f3e
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-29 23:42:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -38,6 +38,9 @@ for label, row in lista.iterrows():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
     chrome_options.add_argument('--no-sandbox')
+
+    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36')
+
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=chrome_options)
 
     driver.refresh()
@@ -45,18 +48,49 @@ for label, row in lista.iterrows():
     driver.get(url.read())
 
     time.sleep(2)
-    driver.find_element_by_id('Email').send_keys(conta)
+    driver.get_screenshot_as_file('screenshot_0.png')
+
+    #print( driver.page_source )
+
+    driver.find_element_by_id('identifierId').send_keys(conta)
     driver.get_screenshot_as_file('screenshot_1.png')
-    driver.find_element_by_id('next').click()
+    driver.find_element_by_id('identifierNext').click()
     time.sleep(2)
     driver.get_screenshot_as_file('screenshot_2.png')
-    driver.find_element_by_id('password').click()
-    driver.find_element_by_id('password').send_keys(row['senha'])
-    driver.find_element_by_id('submit').click()
+    driver.find_element_by_name('password').click()
+    driver.find_element_by_name('password').send_keys(row['senha'])
+    driver.find_element_by_id('passwordNext').click()
     time.sleep(2)
     #driver.find_element_by_id('accept').click() #botao de nova conta G Suite
     time.sleep(2)
     driver.get_screenshot_as_file('screenshot_3.png')
+
+    page = open('page.html','w')
+    texto=driver.page_source
+    texto=texto.encode('utf8')
+    page.write(texto)
+
+    driver.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/ul/li[1]/div/div[2]').click()
+    time.sleep(10)
+    driver.get_screenshot_as_file('screenshot_4.png')
+
+    page = open('page2.html','w')
+    texto=driver.page_source
+    texto=texto.encode('utf8')
+    page.write(texto)
+
+    driver.find_element_by_id('knowledge-preregistered-email-response').send_keys('contato.salesgames@gmail.com')
+    driver.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button').click()
+
+    time.sleep(10)
+
+    page = open('page3.html','w')
+    texto=driver.page_source
+    texto=texto.encode('utf8')
+    page.write(texto)
+
+    driver.get_screenshot_as_file('screenshot_5.png')
+
     driver.find_element_by_id('submit_approve_access').click()
     time.sleep(2)
     print("Inserindo codigo")
