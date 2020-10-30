fe17ddedde80935cbeb7920dc96a457116abc851
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-30 19:57:20 -0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,10 +2,14 @@
 RED='\033[0;31m'
 VERDE='\033[0;32m'
 NC='\033[0m' # No Color
+
+ulimit -n 65000
+
 while [ 1=1 ]
-do
+do 
  clear
  date
+ ulimit -n
  echo -e "${VERDE}CONECTADO"
  echo -e '\033[05;31mPROXY\033[00;37m' 
  echo -e "${NC}$(sudo systemctl status Proxy)"
