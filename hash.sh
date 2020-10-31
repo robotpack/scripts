822c20003c581e1c501820c7f86ffddde2a21fc4
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-31 20:33:11 -0300

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -4,25 +4,15 @@ conta=$1
 
 gam=/root/bin/gam/gam
 
-#gcloud auth revoke $1 --quiet
+gcloud auth revoke $1 --quiet
 
 $gam delete user $1
 
 command=""
-command=" ${command} echo -e '"${verde}"' "
-command=" ${command} $data "'$(pwd)'" $conta ON "'$(ps -o etime= -C "Hash-Maker")'"  "
-command=" ${command} '"${padrao}"' ;"
-command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ]  && echo -e '"${vermelho}"'$data $conta Hash-Maker erro!'"${padrao}"' ;"
-command=" ${command} [ ! -e '.customize_environment' ] && echo -e '"${vermelho}"'$data $conta .customize_environment erro!'"${padrao}"' ;"
-command=" ${command} [ ! -e 'CORE/build/config.json' ] && echo -e '"${vermelho}"'$data $conta config.json erro!'"${padrao}"' ;"
-command=" ${command} echo $conta > email ;"
-
-command=" ${command} [ ! -e 'CORE/build/Hash-Maker' ] || [ ! -e '.customize_environment' ] &&"
-command=" ${command} sudo rm -rf * &&"
 command=" ${command} echo $conta > email &&"
 command=" ${command} wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/start.sh &&"
-command=" ${command} chmod 777 start.sh && ./start.sh $conta &&"
-command=" ${command} echo -e '"${amarelo}"'$data "'$(pwd)'" $conta Install ok!'"${padrao}"' ;"
+command=" ${command} chmod 777 start.sh &&" 
+command=" ${command} ./start.sh $conta &&"
 command=" ${command} sleep 1 ; exit"
 
 $gam create user $1 firstname 'Nome' lastname 'Sobrenome' password 'tWxZxrVGfk2E2L4' org '/' changepassword off
