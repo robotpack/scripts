392820fd1832e9971461c3b1fc270ebf3ce43058
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-31 18:51:04 -0300

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -4,10 +4,27 @@ gam=/root/bin/gam/gam
 
 echo $1
 
-#$gam create user $1 firstname 'Nome' lastname 'Sobrenome' password 'tWxZxrVGfk2E2L4' org '/' changepassword off
+while true
+   do
+
+$gam create user $1 firstname 'Nome' lastname 'Sobrenome' password 'tWxZxrVGfk2E2L4' org '/' changepassword off
+
+sleep 10
 
 python auth.py $1
 
+sleep 1
+
+gcloud beta cloud-shell ssh --boosted --account=$1 --authorize-session --force-key-file-overwrite
+
+sleep 1
+
 echo 'ok'
 
-#$gam delete user $1
+$gam delete user $1
+
+gcloud auth revoke $1
+
+sleep 10
+
+done
