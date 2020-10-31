02f51be85f1ec474787027f356ede50efbe1b77c
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-30 21:46:43 -0300

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt --assume-yes install samba
-chmod 777 /home/
+chmod 777 /root
 wget -N https://raw.githubusercontent.com/robotpack/scripts/master/config.samba && chmod 777 config.samba
 sudo ./config.samba >> /etc/samba/smb.conf
 #sudo /etc/init.d/samba-ad-dc restart
