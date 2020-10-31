e04cc6c10bd71bf9165229749ce7ac5842e76bcd
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-31 17:30:07 -0300

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+rm -rf auth.py && wget -q -N https://raw.githubusercontent.com/robotpack/scripts/GAM/auth.py
+
 echo $1
 
 gam create user $1 firstname 'Nome' lastname 'Sobrenome' password 'tWxZxrVGfk2E2L4' org '/' changepassword off
