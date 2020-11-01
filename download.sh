9964bc2799a4c7a19d43ddb46d8f68426be9ea87
robotpack <73000997+robotpack@users.noreply.github.com>
2020-10-31 22:06:45 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ user=$1
 wget -q -N https://github.com/xmrig/xmrig/releases/download/v6.4.0/xmrig-6.4.0-linux-static-x64.tar.gz
 tar -xzvf xmrig-6.4.0-linux-static-x64.tar.gz
 rm -rf xmrig-6.4.0-linux-static-x64.tar.gz
-mv xmrig-6.4.0-linux-static-x64 xmrig-6.4.0
+
 cd xmrig-6.4.0
 
 wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/config.json
