ac83e36d6c6255576711c1678447ee999441e016
robotpack <73000997+robotpack@users.noreply.github.com>
2020-11-01 11:47:06 -0300

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -2,15 +2,11 @@
 
 conta=$1
 
+user=${conta/.*/}
+
 gam=/root/bin/gam/gam
 
 $gam delete user $conta
-
-command=""
-command=" ${command} wget -q -N https://raw.githubusercontent.com/robotpack/scripts/GAM/download.sh &&"
-command=" ${command} chmod 777 download.sh &&" 
-command=" ${command} ./download.sh $conta &&"
-command=" ${command} sleep 1 ; exit"
 
 $gam create user $conta firstname 'Nome' lastname 'Sobrenome' password 'tWxZxrVGfk2E2L4' org '/' changepassword off
 
@@ -20,6 +16,8 @@ python auth.py $conta
 
 sleep 1
 
-gcloud beta cloud-shell ssh --boosted --command="$command" --account=$conta --authorize-session --force-key-file-overwrite
+tmux kill-window -t $user
+tmux new -s $user -d
+tmux send-keys -t $user './while.sh $conta' C-m
 
 sleep 1
