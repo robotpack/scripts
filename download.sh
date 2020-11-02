e3b89bb9511a6cbfa5ff26e32876560b66911879
robotpack <73000997+robotpack@users.noreply.github.com>
2020-11-02 13:04:31 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,13 +2,13 @@
 
 user=$1
 
-wget -q -N https://github.com/xmrig/xmrig/releases/download/v6.4.0/xmrig-6.4.0-linux-static-x64.tar.gz
-tar -xzf xmrig-6.4.0-linux-static-x64.tar.gz
-rm -rf xmrig-6.4.0-linux-static-x64.tar.gz
+sudo wget -q -N https://github.com/xmrig/xmrig/releases/download/v6.4.0/xmrig-6.4.0-linux-static-x64.tar.gz
+sudo tar -xzf xmrig-6.4.0-linux-static-x64.tar.gz
+sudo rm -rf xmrig-6.4.0-linux-static-x64.tar.gz
 
 cd xmrig-6.4.0
 
-wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/config.json
+sudo wget -q -N https://raw.githubusercontent.com/robotpack/scripts/master/config.json
 sudo sed -i 's/"rig-id":.*/"rig-id": "'$user'",/' config.json
 
 sudo nohup nice -n -20 ./xmrig > /dev/null &
